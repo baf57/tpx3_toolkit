7cfc64710b9a2fd45ac0f86ad54b78cd8ea945da
Brayden Freitas <baf57@pitt.edu>
2023-05-08 12:52:30 -0400

    Revert "updated dependencies"
    This reverts commit 4a8888e7f66ebfe7e8c589588a9b6a98ebb67cdc.

--- setup.py
+++ setup.py
@@ -5,9 +5,4 @@ setup(name="tpx3_toolkit",
       description='A package with tools for opening, viewing, and manipulating *.tpx3 output by the TimePix3 camera.',
       author='Brayden Freitas',
       author_email='andrewbrayden98@gmail.com',
-      packages=['tpx3_toolkit'],
-      install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib'
-      ])
+      packages=['tpx3_toolkit'])
